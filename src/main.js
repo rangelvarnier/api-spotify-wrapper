aa10b65fbd2f2d82446b52d7ba44cbd0773ee9b5
Rangel Varnier <rangel.varnier@gmail.com>
2018-06-02 17:44:58 -0300

    generic search

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import fetch from 'node-fetch'
 
-export const search = () => fetch('http://spotify.com')
+export const search = (query, type) =>
+  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
+    .then(data => data.json())
+    .catch(error => console.log(error))
 export const searchAlbuns = () => { }
 export const searchArtists = () => { }
 export const searchTracks = () => { }
--- test/main.test.js
+++ test/main.test.js
@@ -8,10 +8,14 @@ import {
 
 import fetch from 'node-fetch'
 jest.mock('node-fetch')
+fetch
+  .mockReturnValue(Promise.resolve({ json: () => {
+    return {body: 'json'}
+  }}))
 
 describe('Main', () => {
   describe('Spotfy Wrapper', () => {
-    describe('smoke tests', () => {
+    describe('Smoke tests', () => {
       test('should exist the search method', () => {
         expect(search).toBeDefined()
       })
@@ -37,6 +41,26 @@ describe('Main', () => {
       test('should call fetch function', () => {
         search()
         expect(fetch).toBeCalled()
+      })
+
+      describe('should receive de correct url to fetch', () => {
+        test('passing one type', () => {
+          search('zztop', 'artist')
+          expect(fetch).toHaveBeenCalledWith('https://api.spotify.com/v1/search?q=zztop&type=artist')
+
+          search('zztop', 'album')
+          expect(fetch).toHaveBeenCalledWith('https://api.spotify.com/v1/search?q=zztop&type=album')
+        })
+
+        test('passing more than one type', () => {
+          search('zztop', ['artist, album'])
+          expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=zztop&type=artist')
+        })
+      })
+
+      test('should return the JSON data from the Promise', () => {
+        const artist = search('zztop', 'artist')
+        expect(artist).resolves.toEqual({body: 'json'})
       })
     })
   })
