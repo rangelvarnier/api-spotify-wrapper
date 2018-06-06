3035de7ac6192421dc792146321330ba8484fb3f
Rangel Varnier <rangel.varnier@gmail.com>
2018-06-05 23:17:58 -0300

    webpack configuration for build with babel loader

---
+++ webpack.config.babel.js
@@ -0,0 +1,28 @@
+import { join } from 'path'
+const include = join(__dirname, 'src')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'spotifyWrapper'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+        include
+
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+}
